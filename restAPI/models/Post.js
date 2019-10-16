834499fc040b9c67317ce1341522c644d1c79ed8
Md-Mudassir <mdmudassirpro@gmail.com>
2019-10-16 10:13:09 +0530

    restAPI

--- restAPI/models/Post.js
+++ restAPI/models/Post.js
@@ -16,3 +16,15 @@ const PostSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model("Posts", PostSchema);
+// question: {
+//   type: String,
+//   required: true
+// },
+// correct_answer: {
+//   type: String,
+//   required: true
+// },
+// incorrect_answers: {
+//   type: Array,
+//   required: true
+// }
